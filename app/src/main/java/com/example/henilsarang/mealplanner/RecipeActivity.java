2735682fea0cf848914ae9d20a6e5bf157725e4e
Tiancch <tiancch@shaw.ca>
2018-06-25 16:06:58 -0600

    Added Setting, mealActivity, recipeActivity, myAdapter and mealDetailActivity

--- app/src/main/java/com/example/henilsarang/mealplanner/RecipeActivity.java
+++ app/src/main/java/com/example/henilsarang/mealplanner/RecipeActivity.java
@@ -22,7 +22,7 @@ public class RecipeActivity extends AppCompatActivity {
 
     ListView recipes;
     String[] titles = {"Cauliflower Tikka Masala","No-Pain Lo Mein","One-Pot Farfalle Primavera","One-Pot Spaghetti with Fresh Tomato Sauce","Pan Roasted Pork Chops and Broccoli","Pea and Goat's Cheese Risotto"};
-    String[] descriptions = {"4.6/5 Enticing, Appealing, Finger Licking!", "4.2 Enjoyable", "3.8 Marvelous","4.0/5 Best in Calgary","4.3/5 Delicious","4.7/5 Fantastic Savory"};
+    String[] descriptions = {"4.6/5 Enticing, Appealing, Finger Licking!", "4.2/5 Enjoyable", "3.8/5 Marvelous","4.0/5 Best in Calgary","4.3/5 Delicious","4.7/5 Fantastic Savory"};
     int[] images = {R.drawable.cauliflower_tikka_masala, R.drawable.nopailomein, R.drawable.onepotfarfalleprimavera, R.drawable.onepotspaghettiwithfreshtomatosauce, R.drawable.panroastedporkchopsandbroccoli, R.drawable.peaandgoatscheeserisotto};
 
 
--- app/src/main/java/com/example/henilsarang/mealplanner/ShoppingListActivity.java
+++ app/src/main/java/com/example/henilsarang/mealplanner/ShoppingListActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ShoppingListActivity extends AppCompatActivity {
 
     ListView mListView;
     String[] titles = {"Sour Cream","Vegetable Oil","Garlic Cloves","Baby Potatoes","Spaghetti","Tomato Paste"};
+//    int[] images = {R.drawable.sour_cream, R.drawable.vegetable_oil,R.drawable.garlic_cloves,R.drawable.baby_potatoes,R.drawable.spaghetti,R.drawable.tomato_paste};
     int[] images = {R.drawable.sour_cream, R.drawable.vegetable_oil,R.drawable.garlic_cloves,R.drawable.baby_potatoes,R.drawable.spaghetti,R.drawable.tomato_paste};
     String[] descriptions = {"2 cups", "4 Tbsp", "1 lb","12 oz","20 oz","3 table spoon"};
 
@@ -21,15 +23,19 @@ public class ShoppingListActivity extends AppCompatActivity {
         setContentView(R.layout.shopping_list);
 
             mListView = (ListView) findViewById(R.id.lvShoppingList);
+
+
             MyAdapter myAdapter = new MyAdapter(ShoppingListActivity.this, titles, images, descriptions);
             mListView.setAdapter(myAdapter);
+
             mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
                     Intent mIntent = new Intent(ShoppingListActivity.this, ShoppingDetailActivity.class);
-                    mIntent.putExtra("titles", titles[i]);
-                    mIntent.putExtra("images", images[i]);
-                    mIntent.putExtra("descriptions", descriptions[i]);
+
+                    mIntent.putExtra("title", titles[i]);
+                    mIntent.putExtra("image", images[i]);
+                    mIntent.putExtra("description", descriptions[i]);
                     startActivity(mIntent);
                 }
             });
